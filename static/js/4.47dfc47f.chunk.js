670d7fb3b2f97b7169e4577f0d717ab547cf4a7d
ukorvl <ukorvl@users.noreply.github.com>
2022-12-19 10:16:40 +0000

    deploy: a35de9dc23e5adfffa8fc4711d234547dd1997aa

---
+++ static/js/4.47dfc47f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpproof.market.nil.foundation"]=this["webpackJsonpproof.market.nil.foundation"]||[]).push([[4],{749:function(n,t,o){"use strict";o.r(t);var i=o(120),r=o(1);t.default=function(){return Object(r.jsx)(i.n,{})}}}]);
+//# sourceMappingURL=4.47dfc47f.chunk.js.map
