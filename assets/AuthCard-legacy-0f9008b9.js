1b49ad3dbb966dd3194c555148422730d6e00557
ukorvl <ukorvl@users.noreply.github.com>
2023-06-19 11:55:06 +0000

    deploy: 87b6ee8e1a0fcfaf31efb7b4e477b3811fe980a5

---
+++ assets/AuthCard-legacy-0f9008b9.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-9608b243.js"],(function(e,t){"use strict";var r,s,c;return{setters:[e=>{r=e.p,s=e.bf,c=e.bg}],execute:function(){e("A",(({children:e})=>r(s,{className:c.authCard,children:e})))}}}));
--- assets/AuthCard-legacy-35a7f7e2.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-7269a1b0.js"],(function(e,t){"use strict";var r,s,c;return{setters:[e=>{r=e.p,s=e.bf,c=e.bg}],execute:function(){e("A",(({children:e})=>r(s,{className:c.authCard,children:e})))}}}));
